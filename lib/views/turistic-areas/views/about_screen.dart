4fd172e1cb8482d5f2a66ae4cd8b37bcb9b146c0
unknown <eduard.agapia@gmail.com>
2021-11-17 10:45:00 +0200

    Make the about screen

--- lib/views/turistic-areas/views/about_screen.dart
+++ lib/views/turistic-areas/views/about_screen.dart
@@ -1,7 +1,6 @@
-import 'package:firebase_database/firebase_database.dart';
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:qr_4_all/domain/gal.dart';
 
 class AboutScreen extends StatefulWidget {
   _AboutScreenState createState() => _AboutScreenState();
@@ -10,19 +9,197 @@ class AboutScreen extends StatefulWidget {
 class _AboutScreenState extends State<AboutScreen> {
   @override
   Widget build(BuildContext context) {
+    final double iconSize = MediaQuery.of(context).size.width / 24;
+
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: const Text("About Screen"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: const [
-            Text("Aici sunt detalii despre gal"),
-          ],
-        ),
+      body: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(0, 250, 0, 0),
+            height: MediaQuery.of(context).size.height / 3,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(35.0)),
+              color: Colors.lightGreen,
+              image: DecorationImage(
+                  image: AssetImage("assets/gals/iasi.png"), fit: BoxFit.cover),
+            ),
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0, 4),
+              width: double.infinity,
+              height: 2,
+              child: const Align(
+                alignment: Alignment.bottomCenter,
+                child: Text(
+                  "Nume Oras",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+              ),
+              decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                  color: Color.fromARGB(200, 255, 255, 255)),
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: MediaQuery.of(context).size.height / 13,
+            width: MediaQuery.of(context).size.width - 25,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(20.0)),
+              color: Colors.white,
+            ),
+            child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: [
+                Container(
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage("assets/gals/icons/hospital.png"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage("assets/gals/icons/insta.png"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage("assets/gals/icons/arrow.png"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage("assets/gals/icons/mech.png"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Container(
+                  height: double.infinity,
+                  width: MediaQuery.of(context).size.width / 6,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(15.0)),
+                    color: Colors.white,
+                    image: DecorationImage(
+                        image: AssetImage("assets/gals/icons/www.png"),
+                        fit: BoxFit.cover),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: MediaQuery.of(context).size.height / 6,
+            width: MediaQuery.of(context).size.width - 20,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(25.0)),
+              color: Colors.lightGreen,
+            ),
+            child: Column(children: const <Widget>[
+              ListTile(
+                title: Text(
+                  '\nPlaceholder \n',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15,
+                    fontWeight: FontWeight.w900,
+                  ),
+                ),
+                subtitle: Text(
+                  'Lorem ipsum dolor sit amet, consectetur adipiscing elit, '
+                  'sed do eiusmod tempor incididunt ut labore et dolore '
+                  'magna aliqua. Ut enim ad minim veniam, quis nostrud'
+                  ' exercitation ullamco laboris nisi ut aliquip ex ea '
+                  'commodo consequat. Duis aute irure dolor in reprehenderit '
+                  'in voluptate velit esse cillum dolore eu fugiat nulla '
+                  'pariatur. Excepteur sint occaecat cupidatat non proident,'
+                  ' sunt in culpa qui officia deserunt mollit anim id est '
+                  'laborum.',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12,
+                  ),
+                ),
+              ),
+            ]),
+          ),
+          const SizedBox(height: 10),
+          Flexible(
+            child: Container(
+              width: MediaQuery.of(context).size.width - 12,
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                color: Colors.lightGreen,
+              ),
+              child: Column(children: const <Widget>[
+                ListTile(
+                  title: Text(
+                    '\nPlaceholder \n',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 15,
+                      fontWeight: FontWeight.w900,
+                    ),
+                  ),
+                  subtitle: Text(
+                    '"Sed ut perspiciatis unde omnis iste natus error sit '
+                    'voluptatem accusantium doloremque laudantium, totam rem'
+                    ' aperiam, eaque ipsa quae ab illo inventore veritatis '
+                    'et quasi architecto beatae vitae dicta sunt explicabo. '
+                    'Nemo enim ipsam voluptatem quia voluptas sit aspernatur '
+                    'aut odit aut fugit, sed quia consequuntur magni dolores'
+                    ' eos qui ratione voluptatem sequi nesciunt. Neque porro'
+                    ' quisquam est, qui dolorem ipsum quia dolor sit amet,'
+                    ' consectetur, adipisci velit, sed quia non numquam eius'
+                    ' modi tempora incidunt ut labore et dolore magnam aliquam '
+                    'quaerat voluptatem. Ut enim ad minima veniam, quis nostrum '
+                    'exercitationem ullam corporis suscipit laboriosam, nisi '
+                    'ut aliquid ex ea commodi consequatur? Quis autem vel eum'
+                    ' iure reprehenderit qui in ea voluptate velit esse quam '
+                    'nihil molestiae consequatur, vel illum qui dolorem eum'
+                    ' fugiat quo voluptas nulla pariatur?"',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          ),
+          const SizedBox(height: 10),
+        ],
       ),
     );
   }
