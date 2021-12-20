54cb8e9d65ae2a93df0d427827ee42e402c94faf
EduardAgapia <eduard.agapia@gmail.com>
2021-12-20 16:47:46 +0200

    refactor menu_screen.dart

--- lib/views/menu_screen.dart
+++ lib/views/menu_screen.dart
@@ -16,47 +16,9 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-  final String jSonLocation = 'assets/ro-gals.json';
+  String jSonLocation = 'assets/ro-gals.json';
   List<Gal> _gals = List.empty(growable: true);
   final Set<Marker> _markers = {};
-
-  Future<void> readJson() async {
-    final String response = await rootBundle.loadString(jSonLocation);
-    final Map<String, dynamic> data = await json.decode(response);
-    setState(() {
-      _gals = Gals.fromJson(data).gals;
-    });
-  }
-
-  List<Marker> markers = [//make dinamic
-    const Marker(
-        markerId: MarkerId('Codrii Pascanilor'),
-        position: LatLng(47.249624, 26.720125)),
-    const Marker(
-        markerId: MarkerId('Belcesti-Focuri'),
-        position: LatLng(47.216394, 27.148312)),
-    const Marker(
-        markerId: MarkerId('DEalurile bohotinului'),
-        position: LatLng(46.947622, 27.986547)),
-    const Marker(
-        markerId: MarkerId('Rediu-Prajeni'),
-        position: LatLng(47.232995, 27.500137)),
-    const Marker(
-        markerId: MarkerId('Siret-Moldova'),
-        position: LatLng(47.362216, 26.689119)),
-    const Marker(
-        markerId: MarkerId('Stefan cel Mare'),
-        position: LatLng(47.204105, 27.602336)),
-    const Marker(
-        markerId: MarkerId('Stejarii Argintii'),
-        position: LatLng(47.144530, 27.520261)),
-    const Marker(
-        markerId: MarkerId('Valea Prutului'),
-        position: LatLng(47.281021, 27.518267)),
-    const Marker(
-        markerId: MarkerId('Colinele Iasului'),
-        position: LatLng(47.060847, 27.568177)),
-  ];
 
   @override
   void initState() {
@@ -125,4 +87,49 @@ class _MenuScreenState extends State<MenuScreen> {
       ),
     );
   }
+
+  Future<void> readJson() async {
+    if(enSystemLanguage()){
+      jSonLocation = 'assets/en-gals.json';
+    }
+    final String response = await rootBundle.loadString(jSonLocation);
+    final Map<String, dynamic> data = await json.decode(response);
+    setState(() {
+      _gals = Gals.fromJson(data).gals;
+    });
+  }
+
+  bool enSystemLanguage() {
+    return false;
+  }
+
+  List<Marker> markers = [//make dinamic
+    const Marker(
+        markerId: MarkerId('Codrii Pascanilor'),
+        position: LatLng(47.249624, 26.720125)),
+    const Marker(
+        markerId: MarkerId('Belcesti-Focuri'),
+        position: LatLng(47.216394, 27.148312)),
+    const Marker(
+        markerId: MarkerId('DEalurile bohotinului'),
+        position: LatLng(46.947622, 27.986547)),
+    const Marker(
+        markerId: MarkerId('Rediu-Prajeni'),
+        position: LatLng(47.232995, 27.500137)),
+    const Marker(
+        markerId: MarkerId('Siret-Moldova'),
+        position: LatLng(47.362216, 26.689119)),
+    const Marker(
+        markerId: MarkerId('Stefan cel Mare'),
+        position: LatLng(47.204105, 27.602336)),
+    const Marker(
+        markerId: MarkerId('Stejarii Argintii'),
+        position: LatLng(47.144530, 27.520261)),
+    const Marker(
+        markerId: MarkerId('Valea Prutului'),
+        position: LatLng(47.281021, 27.518267)),
+    const Marker(
+        markerId: MarkerId('Colinele Iasului'),
+        position: LatLng(47.060847, 27.568177)),
+  ];
 }
