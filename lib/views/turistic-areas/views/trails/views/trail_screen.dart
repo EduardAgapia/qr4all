16f6cc19df0b02f4ce19b3d6d51cb68abfa6f7b8
EduardAgapia <eduard.agapia@gmail.com>
2022-02-07 22:16:05 +0200

    solved another thing

--- lib/views/turistic-areas/views/trails/views/trail_screen.dart
+++ lib/views/turistic-areas/views/trails/views/trail_screen.dart
@@ -54,8 +54,7 @@ class _TrailScreenState extends State<TrailScreen> {
                   child: Column(children: <Widget>[
                     ListTile(
                       title: Text(
-                        '\n' +
-                        trail.name.toString(),
+                        '\n' + trail.name.toString(),
                         style: TextStyle(
                           color: Colors.white,
                           fontSize: MediaQuery.of(context).size.height / 30,
@@ -75,7 +74,8 @@ class _TrailScreenState extends State<TrailScreen> {
                   ]),
                 ),
                 SizedBox(height: MediaQuery.of(context).size.height / 13),
-                Container(//here was a flexible
+                Container(
+                  //here was a flexible
                   padding: const EdgeInsets.fromLTRB(2, 0, 2, 0),
                   height: MediaQuery.of(context).size.height / 1.65,
                   decoration: const BoxDecoration(
@@ -95,15 +95,69 @@ class _TrailScreenState extends State<TrailScreen> {
                           textAlign: TextAlign.left,
                         ),
                         subtitle: Text(
-                          trail.description.toString(),
+                          'Sed ut perspiciatis unde omnis iste natus error sit '
+                              'voluptatem accusantium doloremque laudantium, '
+                              'totam rem aperiam, eaque ipsa quae ab illo '
+                              'inventore veritatis et quasi architecto beatae '
+                              'vitae dicta sunt explicabo. Nemo enim ipsam '
+                              'voluptatem quia voluptas sit aspernatur aut odit '
+                              'aut fugit, sed quia consequuntur magni dolores '
+                              'eos qui ratione voluptatem sequi nesciunt. '
+                              'Neque porro quisquam est, qui dolorem ipsum quia '
+                              'dolor sit amet, consectetur, adipisci velit, sed '
+                              'quia non numquam eius modi tempora incidunt ut '
+                              'labore et dolore magnam aliquam quaerat voluptatem.'
+                          '\n',
                           style: TextStyle(
                             color: Colors.white,
-                            fontSize: MediaQuery.of(context).size.height / 45,
+                            fontSize: MediaQuery.of(context).size.height / 80,
                           ),
                         ),
                       ),
-                      SizedBox(
-                          height: MediaQuery.of(context).size.height / 50),
+                      ListTile(
+                        leading: Icon(Icons.assistant_navigation, color: Colors.black),
+                        title: Text(
+                         '• Locatia 1' + '\n'
+                         '• Locatia 2' + '\n'
+                         '• Locatia 3' + '\n'
+                         '• Locatia 4' + '\n'
+                         '• Locatia 5' + '\n'
+                         '• Locatia 6' + '\n'
+                         '• Locatia 7' + '\n'
+                         '• Locatia 8' + '\n'
+                         '• Locatia 9' + '\n'
+                         '• Locatia 10' + '\n'
+                         '• Locatia 11' + '\n',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.height / 65,
+                            fontWeight: FontWeight.w800,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                      ),
+                      ListTile(
+                        title: Text(
+                         'Descriere Generala' + '\n',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.height / 35,
+                            fontWeight: FontWeight.w800,
+                          ),
+                          textAlign: TextAlign.left,
+                        ),
+                        subtitle: Text(
+                          'Distanta totala a traseului: 53KM'+'\n'
+                          'Tipul traseului: cicloturism'+'\n'
+                          'Dificultate: medie'+'\n'
+                          'Diferenta de nivel: 234m'+'\n',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: MediaQuery.of(context).size.height / 80,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: MediaQuery.of(context).size.height / 50),
                       Align(
                         child: SizedBox(
                           height: MediaQuery.of(context).size.height / 11,
@@ -111,15 +165,13 @@ class _TrailScreenState extends State<TrailScreen> {
                               scrollDirection: Axis.horizontal,
                               children: [
                                 SizedBox(
-                                    width:
-                                        MediaQuery.of(context).size.width /
-                                            3.2),
+                                    width: MediaQuery.of(context).size.width /
+                                        3.2),
                                 Container(
-                                  width:
-                                      MediaQuery.of(context).size.width / 13,
+                                  width: MediaQuery.of(context).size.width / 13,
                                   decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(25.0)),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(25.0)),
                                   ),
                                   child: GestureDetector(
                                     onTap: () {
@@ -137,13 +189,15 @@ class _TrailScreenState extends State<TrailScreen> {
                                     ),
                                   ),
                                 ),
-                                SizedBox(width: MediaQuery.of(context).size.width / 25),
+                                SizedBox(
+                                    width:
+                                        MediaQuery.of(context).size.width / 25),
                                 Container(
                                   width:
                                       MediaQuery.of(context).size.width / 8.5,
                                   decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(25.0)),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(25.0)),
                                   ),
                                   child: GestureDetector(
                                     onTap: () {
@@ -160,13 +214,14 @@ class _TrailScreenState extends State<TrailScreen> {
                                   ),
                                 ),
                                 SizedBox(
-                                    width: MediaQuery.of(context).size.width / 25),
+                                    width:
+                                        MediaQuery.of(context).size.width / 25),
                                 Container(
                                   width:
                                       MediaQuery.of(context).size.width / 10.5,
                                   decoration: const BoxDecoration(
-                                    borderRadius: BorderRadius.all(
-                                        Radius.circular(25.0)),
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(25.0)),
                                   ),
                                   child: GestureDetector(
                                     onTap: () {
